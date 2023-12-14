5a5f69f00e33cd5bb59b4a98e175560e7b712549
alexandremulina <alexandremulina@hotmail.com>
2023-12-14 00:41:55 -0300

    d3

--- api/daythree.go
+++ api/daythree.go
@@ -107,17 +107,129 @@ func checkAdjacentSymbols(stringNumber string, coord []int, indexLine int, lines
 }
 
 func rangesIntersect(coord []int, symbolRanges [][]int) bool {
-	fmt.Println("coord:", coord, "symbolRanges:", symbolRanges)
+	// fmt.Println("coord:", coord, "numbersRange:", symbolRanges)
 	for _, symbolRange := range symbolRanges {
 
 		if (symbolRange[0] >= coord[0] && symbolRange[0] <= coord[1]) ||
 			(symbolRange[1] >= coord[0] && symbolRange[1] <= coord[1]) {
-			fmt.Println("symbolRange:", symbolRange[0], symbolRange[1])
-			fmt.Println("coord:", coord[0], coord[1])
-			fmt.Println("return True")
+			// fmt.Println("symbolRange:", symbolRange[0], symbolRange[1])
+			// fmt.Println("coord:", coord[0], coord[1])
+			// fmt.Println("return True")
 			return true
 		}
 	}
 
 	return false
+}
+
+//Part 2
+
+//Check for Numbers Adjancts to Symbol *
+
+func DayThreePart2(c *gin.Context) {
+	fileContent, err := FileCheck(c)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	lines, err := util.ReadFileContent(fileContent)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": "Error reading file content",
+		})
+		return
+	}
+	totalGears := 0
+	re := regexp.MustCompile(`(?i)\*`)
+	// numbersRegex := regexp.MustCompile(`(\d+)`)
+	for i := 0; i < len(lines); i++ {
+		lineSymbolsIndexs := re.FindAllStringIndex(lines[i], -1)
+		lineSymbols := re.FindAllString(lines[i], -1)
+		// fmt.Println("lineSymbolsIndexs:", lineSymbolsIndexs)
+		// umbersIndexes := numbersRegex.FindAllStringIndex(lines[i], -1)
+		// fmt.Println("numbers:", numbersRegex.FindAllString(lines[i], -1))
+		// fmt.Println("numbersIndexes:", umbersIndexes)
+		for index, lineSymbol := range lineSymbols {
+			// fmt.Println("lineSymbolsIndexs", lineSymbolsIndexs[index])
+			numbers := checkAdjacentNumbersFromSymbols(lineSymbol, lineSymbolsIndexs[index], i, lines)
+			fmt.Println("numbers:", numbers, "index:", i)
+			intNumbers := convertStringArrayToInt(numbers)
+
+			if len(intNumbers) > 1 {
+				multiplied := intNumbers[0] * intNumbers[1]
+				totalGears += multiplied
+
+			}
+		}
+
+	}
+	fmt.Println("totalGears:", totalGears)
+
+	c.JSON(200, gin.H{
+		"result": lines,
+		"status": 200,
+	})
+}
+
+func checkAdjacentNumbersFromSymbols(stringSymbol string, coord []int, indexLine int, lines []string) []string {
+	arrString := []string{}
+	re := regexp.MustCompile(`(\d+)`)
+
+	// Before Line
+	if indexLine > 0 {
+		beforeNumbers := re.FindAllStringIndex(lines[indexLine-1], -1)
+		if len(beforeNumbers) > 0 && beforeNumbers != nil {
+			for _, coords := range rangeIntersect(coord, beforeNumbers) {
+				arrString = append(arrString, lines[indexLine-1][coords[0]:coords[1]])
+			}
+		}
+	}
+
+	// Current Line
+	if indexLine >= 0 && indexLine < len(lines) {
+		currentNumbers := re.FindAllStringIndex(lines[indexLine], -1)
+		if len(currentNumbers) > 0 && currentNumbers != nil {
+			for _, coords := range rangeIntersect(coord, currentNumbers) {
+				arrString = append(arrString, lines[indexLine][coords[0]:coords[1]])
+			}
+		}
+	}
+
+	// After Line
+	if indexLine < len(lines)-1 {
+		afterNumbers := re.FindAllStringIndex(lines[indexLine+1], -1)
+		if len(afterNumbers) > 0 && afterNumbers != nil {
+			for _, coords := range rangeIntersect(coord, afterNumbers) {
+				arrString = append(arrString, lines[indexLine+1][coords[0]:coords[1]])
+			}
+		}
+	}
+
+	return arrString
+}
+
+func rangeIntersect(coord []int, numbersRanges [][]int) [][]int {
+	var result [][]int
+	for _, numbersRange := range numbersRanges {
+		if (numbersRange[0] >= coord[0] && numbersRange[0] <= coord[1]) ||
+			(numbersRange[1] >= coord[0] && numbersRange[1] <= coord[1]) ||
+			(numbersRange[0] <= coord[0] && numbersRange[1] >= coord[1]) {
+			result = append(result, numbersRange)
+		}
+	}
+	return result
+}
+
+func convertStringArrayToInt(strNumbers []string) []int {
+	intNumbers := make([]int, len(strNumbers))
+	for i, strNum := range strNumbers {
+		num, err := strconv.Atoi(strNum)
+		if err == nil {
+			intNumbers[i] = num
+		}
+	}
+	return intNumbers
 }
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	r.GET("/", api.HealthCheck)
 	r.POST("/dayOne", api.DayOne)
 	r.POST("/dayTwo", api.DayTwo)
-	r.POST("/dayThree", api.DayThree)
+	r.POST("/dayThree", api.DayThreePart2)
 
 	r.Run(":9002")
 }
