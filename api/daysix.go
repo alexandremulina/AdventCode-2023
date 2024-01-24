d3ca8c57ca7a1c2d8d8e392a27c0495dfdff4a65
alexandremulina <alexandremulina@hotmail.com>
2024-01-23 21:32:23 -0300

    day 6 part one and part two

--- api/daysix.go
+++ api/daysix.go
@@ -35,15 +35,26 @@ func DaySix(c *gin.Context) {
 	// 	lines[0], -1), "distance:", regexDistance.FindAllString(lines[1], -1))
 	//empty array
 	emptyArray := []int{}
+	emptyArray2 := []int{}
 	timeArray := regexTime.FindAllString(lines[0], -1)
 	distanceArray := regexDistance.FindAllString(lines[1], -1)
-	fmt.Println("timeArray:", strings.Join(timeArray, ""), "distanceArray:", strings.Join(distanceArray, ""))
-	//Part two
+	// fmt.Println("timeArray:", strings.Join(timeArray, ""), "distanceArray:", strings.Join(distanceArray, ""))
+	// Part two
+	newTime, _ := strconv.Atoi(strings.Join(timeArray, ""))
+	newDistance, _ := strconv.Atoi(strings.Join(distanceArray, ""))
+	emptyArray2 = append(emptyArray2, boatMilimeterPartTwo(newTime, newDistance))
+	multplieCounter2 := 1
+	for _, empty2 := range emptyArray2 {
+		multplieCounter2 *= empty2
+	}
+	// fmt.Println("multplieCounter2:", multplieCounter2)
+	//Part One
 	for index, time := range timeArray {
 		//Covert to numbers
 		time, _ := strconv.Atoi(time)
 		distance, _ := strconv.Atoi(distanceArray[index])
 		emptyArray = append(emptyArray, boatMilimeter(time, distance))
+
 	}
 	fmt.Println("emptyArray:", emptyArray)
 	multplieCounter := 1
@@ -62,11 +73,11 @@ func DaySix(c *gin.Context) {
 
 func boatMilimeter(time int, distance int) int {
 	counter := 0
-	fmt.Println("time:", time, "distance:", distance)
+	// fmt.Println("time:", time, "distance:", distance)
 	for i := 1; i <= time; i++ {
 		holder := i
-		distanceAfterHold := holder * (time - holder)
-		fmt.Println("distanceAfterHold:", distanceAfterHold)
+		distanceAfterHold := holder * (time - holder) // ---> holder*time - holder*holder = distanceAfterHold
+		// fmt.Println("distanceAfterHold:", distanceAfterHold)
 		if distanceAfterHold > distance {
 			counter++
 			// fmt.Println("counter:", counter)
@@ -76,4 +87,32 @@ func boatMilimeter(time int, distance int) int {
 	return counter
 }
 
-//Part 2
+// Part 2
+func boatMilimeterPartTwo(time int, distance int) int {
+	// counter := 0
+	var firstHolder int
+	var lasttHolder int
+	for i := 1; i <= time; i++ {
+		holder := i
+		distanceAfterHold := holder * (time - holder)
+		if distanceAfterHold > distance {
+			firstHolder = holder
+			break
+		}
+
+	}
+	for j := time; j >= 0; j-- {
+		holderTwo := j
+		distanceAfterHold := holderTwo * (time - holderTwo)
+		if distanceAfterHold > distance {
+			lasttHolder = holderTwo
+			break
+		}
+	}
+	fmt.Println("firstHolder:", firstHolder, "lasttHolder:", lasttHolder)
+
+	fmt.Println("FinalCounter:", lasttHolder-firstHolder+1)
+	finalCounter := lasttHolder - firstHolder + 1
+	return finalCounter
+
+}
